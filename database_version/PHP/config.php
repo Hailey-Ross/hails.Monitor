d2f9a7e6c448d5ec5a7da662d90aa2d74a502595
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2024-10-17 16:30:16 -0600

    initial boop

---
+++ database_version/PHP/config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php - Store this file in a non-web-accessible directory
+define('DB_SERVER', 'DB-SERVER-URL');
+define('DB_USERNAME', 'YOUR-USERNAME-HERE');
+define('DB_PASSWORD', 'YOUR-PASS-HERE');
+define('DB_NAME', 'YOUR-DB-NAME');
+define('API_KEY', 'YOUR-API-KEY'); // Add the API key here
+?>

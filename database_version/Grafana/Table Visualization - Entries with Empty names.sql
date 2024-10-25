caf9cc404022480e14e55bd9f5447a642659cc7b
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2024-10-25 17:02:48 -0600

    initial boopening

---
+++ database_version/Grafana/Table Visualization - Entries with Empty names.sql
@@ -0,0 +1,8 @@
+SELECT 
+    avatar_name AS Name,
+    region_name AS Region,
+    last_seen AS Time,
+    avatar_key AS UUID
+FROM avatar_visits
+WHERE LOWER(avatar_name) LIKE ''
+ORDER BY last_seen DESC;

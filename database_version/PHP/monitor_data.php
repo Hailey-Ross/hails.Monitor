186c3fc7d96c4f8cf46cb558031d84f70438bfe5
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2026-03-26 23:58:03 -0600

    Update monitor_data.php

--- database_version/PHP/monitor_data.php
+++ database_version/PHP/monitor_data.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['monitor_logged_in']) || $_SESSION['monitor_logged_in'] !==
 }
 
 define('ALLOW_CONFIG_INCLUDE', true);
-require_once '/usr/www/mtnbound/secure/config.php';
+require_once '/path/to/your/hosted/config.php';
 
 function db(): PDO
 {
